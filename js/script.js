b8fa31dfc7ad51a6024152eb36c2403bcb93e623
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-05-20 12:03:57 -0500

    did JSLint stuff for script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log('You\'re viewing the console! Good job!');
+console.log("You're viewing the console! Good job!");
 
 var quotes = [
     "Here is a cool quote",
@@ -10,7 +10,7 @@ var quotes = [
     "You can become a software developer",
     "Customize this site and make it your own!",
     "Software development is for everyone!"
-]
+];
 var authors = [
     "Author 1",
     "Author 2",
@@ -21,7 +21,7 @@ var authors = [
     "Wise person",
     "Alan",
     "Someone"
-]
+];
 
 var count = 1;
 
@@ -29,16 +29,20 @@ var numberOfClicks = 0;
 
 function newQuote() {
     var randomNumber = Math.floor(Math.random() * quotes.length);
-    document.getElementById('quoteText').innerHTML = "\"" + quotes[randomNumber] + "\"";
-    document.getElementById('quoteAuthor').innerHTML = authors[randomNumber];
+    document.getElementById("quoteText").innerHTML = "\"";
+    document.getElementById("quoteText").innerHTML += quotes[randomNumber];
+    document.getElementById("quoteText").innerHTML += "\"";
+    document.getElementById("quoteAuthor").innerHTML = authors[randomNumber];
     count = count + 1;
-    document.getElementById('quoteCount').innerHTML = "Number of quotes shown: " + count;
-    console.log('newQuote() function was called');
+    document.getElementById("quoteCount").innerHTML = "Number of quotes";
+    document.getElementById("quoteCount").innerHTML += "shown: ";
+    document.getElementById("quoteCount").innerHTML += count;
+    console.log("newQuote() function was called");
 }
 
 function countClick() {
     numberOfClicks = numberOfClicks + 1;
-    console.log('You have clicked the page ' + numberOfClicks + ' times.');
+    console.log("You have clicked the page " + numberOfClicks + " times.");
 }
 
 function exampleAlert() {
